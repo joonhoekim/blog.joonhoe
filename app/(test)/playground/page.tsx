d34c4d2531b47d8cf85ea7be7acd341799a860cd
joonhoekim <26rote@gmail.com>
2025-04-03 08:05:53 +0900

    fix: import index.css

--- app/(test)/playground/page.tsx
+++ app/(test)/playground/page.tsx
@@ -1,4 +1,5 @@
 import PlaygroundApp from '@/lexical-use-client/packages/lexical-playground/src/App';
+import '@/lexical-use-client/packages/lexical-playground/src/index.css';
 
 export default function Playground() {
   return <PlaygroundApp />;
