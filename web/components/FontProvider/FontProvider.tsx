76a0e215478d8a84775b86df918127c9d57c9b87
joonhoekim <26rote@gmail.com>
2025-05-13 13:11:15 +0000

    feat: local font supported

--- web/components/FontProvider/FontProvider.tsx
+++ web/components/FontProvider/FontProvider.tsx
@@ -71,79 +71,81 @@ import localFont from "next/font/local";
  * ```
  */
 
+// Inter 폰트 설정
 export const inter = localFont({
-	src: [
-		{
-			path: "./fonts/Inter-Regular.woff2",
-			weight: "400",
-			style: "normal",
-		},
-		{
-			path: "./fonts/Inter-Medium.woff2",
-			weight: "500",
-			style: "normal",
-		},
-		{
-			path: "./fonts/Inter-Bold.woff2",
-			weight: "700",
-			style: "normal",
-		},
-	],
-	variable: "--font-inter",
+  src: [
+    {
+      path: "../../public/fonts/Inter-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Inter-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Inter-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-inter",
 });
 
+// Geist Sans 폰트 설정
 export const geistSans = localFont({
-	src: [
-		{
-			path: "./fonts/GeistSans-Regular.woff2",
-			weight: "400",
-			style: "normal",
-		},
-		{
-			path: "./fonts/GeistSans-Medium.woff2",
-			weight: "500",
-			style: "normal",
-		},
-		{
-			path: "./fonts/GeistSans-Bold.woff2",
-			weight: "700",
-			style: "normal",
-		},
-	],
-	variable: "--font-geist-sans",
+  src: [
+    {
+      path: "../../public/fonts/GeistSans-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/GeistSans-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/GeistSans-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-geist-sans",
 });
 
+// Geist Mono 폰트 설정
 export const geistMono = localFont({
-	src: [
-		{
-			path: "./fonts/GeistMono-Regular.woff2",
-			weight: "400",
-			style: "normal",
-		},
-		{
-			path: "./fonts/GeistMono-Medium.woff2",
-			weight: "500",
-			style: "normal",
-		},
-		{
-			path: "./fonts/GeistMono-Bold.woff2",
-			weight: "700",
-			style: "normal",
-		},
-	],
-	variable: "--font-geist-mono",
+  src: [
+    {
+      path: "../../public/fonts/GeistMono-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/GeistMono-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/GeistMono-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-geist-mono",
 });
 
 interface FontProviderProps {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export function FontProvider({ children }: FontProviderProps) {
-	return (
-		<div
-			className={`${inter.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
-		>
-			{children}
-		</div>
-	);
+  return (
+    <div
+      className={`${inter.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}>
+      {children}
+    </div>
+  );
 }
