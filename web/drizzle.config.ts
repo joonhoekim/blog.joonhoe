4372f174cad531510ba28c1d13f57a5b813c9f92
joonhoekim <26rote@gmail.com>
2025-05-13 13:10:59 +0000

    chore: add logging for NO DATABASE_URL

--- web/drizzle.config.ts
+++ web/drizzle.config.ts
@@ -1,11 +1,18 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+const databaseUrl = process.env.DATABASE_URL;
+if (!databaseUrl) {
+  throw new Error('See your .env file for DATABASE_URL');
+} else {
+  console.log('Got DATABASE_URL successfully from .env file');
+}
+
 export default defineConfig({
   out: './db/migrations',
   schema: './db/schema/*.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL as string,
+    url: databaseUrl,
   },
 });
